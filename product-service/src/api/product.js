960b5a77d555dc54f7f986633a9d3716c01b1266
oktavian aji <oktavian.aji@gmail.com>
2022-05-25 11:06:32 +0700

    add middleware upload image to api create product

--- product-service/src/api/product.js
+++ product-service/src/api/product.js
@@ -1,12 +1,22 @@
 import { ProductServices } from "../services";
 import { responseAPI } from "../utils";
+import { uploadImage } from "./middleware";
 
 export default (app) => {
   const service = new ProductServices();
 
-  app.post("/product", async (req, res, next) => {
+  app.post("/product", uploadImage.single("imageUrl"), async (req, res, next) => {
     try {
-      const { status, data, message } = await service.CreateProduct(req.body);
+      // const objectBody = {
+      //   name: req.body.name,
+      //   description: req.body.description,
+      //   color: req.body.color,
+      //   size: req.body.size,
+      //   price: req.body.price,
+      //   categories: [req.body.categories],
+      //   stock: req.body.stock,
+      // }
+      const { status, data, message } = await service.CreateProduct(req.body, req.file);
 
       responseAPI(res, status, data, message);
     } catch (error) {
