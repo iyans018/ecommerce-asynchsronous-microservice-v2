813bd0890950b66f5102bc19d0114a0fc33de795
oktavian aji <oktavian.aji@gmail.com>
2022-05-25 09:28:09 +0700

    create product schema

---
+++ product-service/src/database/models/Product.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  name: { type: String },
+  description: { type: String },
+  color: { type: String },
+  size: { typre: String },
+  price: { type: Number },
+  imageUrl: { type: String },
+  categories: [{ type: String }],
+  stock: { type: Number }
+}, { timestamps: true });
+
+const Product = mongoose.model("Product", ProductSchema);
+
+export default Product;
