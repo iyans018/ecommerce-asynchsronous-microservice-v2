1ce8dc99e276447abfdb0bc6e7b538a8355c7d17
oktavian aji <oktavian.aji@gmail.com>
2022-05-29 08:55:53 +0700

    create pub/sub event for empty the cart after user create an order

--- order-service/src/api/order.js
+++ order-service/src/api/order.js
@@ -1,13 +1,18 @@
+import redis from "redis";
+
 import { OrderServices } from "../services";
 import { responseAPI } from "../utils";
 import { uploadImage, verifyToken, isAdmin } from "./middleware";
 
 export default (app) => {
   const service = new OrderServices();
+  const publisher = redis.createClient();
 
   app.post("/order", verifyToken, async (req, res, next) => {
     try {
       const { status, data, message } = await service.CreateOrder(req.user, req.body);
+
+      publisher.publish("EMPTY_CART", JSON.stringify({ cart: data.cart }));
 
       return responseAPI(res, status, data, message);
     } catch (error) {
